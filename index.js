20a1fa6f2f4a43d87455fdce65ec2f68a35dfdf8
Vicki Bealman <drbealman@gmail.com>
2021-05-15 11:01:49 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+const port = process.env.port || 3000
+
+app.get('/api', (req, res) => {
+    res.status(200).json({api: 'version 1'})
+})
+
+app.listen(port, () => console.log('server started on port', port))
